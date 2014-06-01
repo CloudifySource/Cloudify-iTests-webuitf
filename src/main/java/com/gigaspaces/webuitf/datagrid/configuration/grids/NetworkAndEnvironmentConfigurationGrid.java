e61ff1ddd05b3c2d3634fea8a0d7dfee80ea7663
evgenyf <evgenyf@gigaspaces.com>
2014-06-01 17:16:14 +0300

    GS-11760 added test for checking jmx service url

--- src/main/java/com/gigaspaces/webuitf/datagrid/configuration/grids/NetworkAndEnvironmentConfigurationGrid.java
+++ src/main/java/com/gigaspaces/webuitf/datagrid/configuration/grids/NetworkAndEnvironmentConfigurationGrid.java
@@ -23,6 +23,7 @@ public class NetworkAndEnvironmentConfigurationGrid extends ConfigurationGrid{
     private final String homeDirectory;
     private final String hostName;
     private final int rmiRegistryPort;
+    private final String jmxServiceUrl;
 
     public NetworkAndEnvironmentConfigurationGrid( AjaxUtils helper ){
         super( helper, ID );
@@ -30,6 +31,7 @@ public class NetworkAndEnvironmentConfigurationGrid extends ConfigurationGrid{
         WebElement homeDirectoryWebElement = thisGridElement.findElement(By.id(ID + "_homeDirectory"));
         WebElement hostNameWebElement = thisGridElement.findElement(By.id(ID + "_hostName"));
         WebElement rmiRegistryPortWebElement = thisGridElement.findElement(By.id(ID + "_rmiRegistryPort"));
+        WebElement jmxServiceUrlWebElement = thisGridElement.findElement(By.id(ID + "_jmxServiceUrl"));
 
         String homeDirectoryLocal = getValue(homeDirectoryWebElement);
         homeDirectory = homeDirectoryLocal.toLowerCase().equals( "n/a" ) ? null : homeDirectoryLocal;
@@ -37,6 +39,8 @@ public class NetworkAndEnvironmentConfigurationGrid extends ConfigurationGrid{
         hostName = hostNameLocal.toLowerCase().equals( "n/a" ) ? null : hostNameLocal;
         String rmiRegistryPortStr = getValue(rmiRegistryPortWebElement);
         rmiRegistryPort = rmiRegistryPortStr.toLowerCase().equals( "n/a" ) ? -1 : Integer.parseInt( rmiRegistryPortStr );
+        String jmxServiceUrlLocal = getValue(jmxServiceUrlWebElement);
+        jmxServiceUrl = jmxServiceUrlLocal.toLowerCase().equals( "n/a" ) ? null : jmxServiceUrlLocal;
     }
 
     public String getHomeDirectory() {
@@ -49,5 +53,9 @@ public class NetworkAndEnvironmentConfigurationGrid extends ConfigurationGrid{
 
     public int getRmiRegistryPort() {
         return rmiRegistryPort;
+    }
+
+    public String getJmxServiceUrl() {
+        return jmxServiceUrl;
     }
 }
